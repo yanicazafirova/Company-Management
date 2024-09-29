7aeede0b23eae0f3adf5e52b7d5b3c27958b12a0
Yanica Zafirova <yanica_zafirova@abv.bg>
2024-09-29 10:57:43 +0300

    Login and register functionality

---
+++ laravel/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        Log::info($request);
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'User registered successfully'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            $user = Auth::user();
+            return response()->json(['message' => 'Login successful', 'user' => $user], 200);
+        }
+
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
+}
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('app');
 });
+
+Route::post('/users/register', [AuthController::class, 'register']);
+Route::post('/users/login', [AuthController::class, 'login']);
